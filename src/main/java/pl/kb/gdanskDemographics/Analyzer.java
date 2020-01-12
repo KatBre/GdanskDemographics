92be9e672848d3fc3322915e256770e84e42e20d
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-01-12 15:07:33 +0100

    Added loading of data

---
+++ src/main/java/pl/kb/gdanskDemographics/Analyzer.java
@@ -0,0 +1,4 @@
+package pl.kb.gdanskDemographics;
+
+public class Analyzer {
+}
--- src/main/java/pl/kb/gdanskDemographics/MainApp.java
+++
@@ -1,7 +0,0 @@
-package pl.kb.gdanskDemographics;
-
-public class MainApp {
-    public static void main(String[] args) {
-
-    }
-}
